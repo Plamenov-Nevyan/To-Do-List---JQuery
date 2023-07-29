43b442a4cd24106704a6553136fbad228d319218
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-07-29 20:08:39 +0300

    Added task completion functionality and completed tasks counter

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 $(document).ready(function(){
-    if(!sessionStorage.getItem('taskLists')){
-        sessionStorage.setItem('taskLists', JSON.stringify({
-            important: [],
-            notImportant: []
-        }))
-    }else {
-    populateLists(JSON.parse(sessionStorage.getItem('taskLists')))
-   }
+    let counter
+    initSessionValues()
 
-    $(".add-btn").on('click', function(event){
-        event.preventDefault()
-        let taskName = $("#taskName").val()
-        let importance = $("input[name=importance]:checked", '.add-form').val()
-        if(taskName !== ''){
-            let taskLists = JSON.parse(sessionStorage.getItem('taskLists'))
-            let taskId = createUniqueId()
-            if(importance === 'important'){
-                $(".important-list").append(createTaskElement(taskName, taskId))
-                taskLists.important = [...taskLists.important, {taskName, taskId}]
-            }else{
-                $(".not-important-list").append(createTaskElement(taskName, taskId))
-                taskLists.notImportant = [...taskLists.notImportant, {taskName, taskId}]
-            }
-            sessionStorage.setItem('taskLists', JSON.stringify(taskLists))
-        }
-    })
+    $(".add-btn").on('click', (event) => {createTask(event)})
 
    function createTaskElement(taskName, taskId){
     let li = $(`<li id=${taskId}></li>`)
@@ -32,7 +10,7 @@ $(document).ready(function(){
     let divBtns = $(`<div class="buttons"></div>`)
     let deleteBtn = $(`<button class="delete-btn">Delete</button>`).on('click', function(event){deleteConfirmation(event)})
     let editBtn =  $(`<button class="edit-btn">Edit</button>`).on('click', function(event){editFormDialog(event)})
-    let completeBtn =  $(`<button class="complete-btn">Complete</button>`)
+    let completeBtn =  $(`<button class="complete-btn">Complete</button>`).on('click', function(){completeTask($(li).attr('id'))})
     $(divBtns).append(deleteBtn)
     $(divBtns).append(editBtn)
     $(divBtns).append(completeBtn)
@@ -81,22 +59,64 @@ $(document).ready(function(){
     populateLists(taskLists)
    }
 
+   function createTask(event){
+    event.preventDefault()
+    let taskName = $("#taskName").val()
+    let importance = $("input[name=importance]:checked", '.add-form').val()
+    if(taskName !== ''){
+        let taskLists = JSON.parse(sessionStorage.getItem('taskLists'))
+        let taskId = createUniqueId()
+        if(importance === 'important'){
+            $(".important-list").append(createTaskElement(taskName, taskId))
+            taskLists.important = [...taskLists.important, {taskName, taskId}]
+        }else{
+            $(".not-important-list").append(createTaskElement(taskName, taskId))
+            taskLists.notImportant = [...taskLists.notImportant, {taskName, taskId}]
+        }
+        sessionStorage.setItem('taskLists', JSON.stringify(taskLists))
+    }
+   }
+
+   function completeTask(taskId){
+    let taskLists = JSON.parse(sessionStorage.getItem('taskLists'))
+    if(taskLists.notImportant.some(task => task.taskId === taskId)){
+        let task = taskLists.notImportant.find(task => task.taskId === taskId)
+        taskLists.notImportant.splice(taskLists.notImportant.indexOf(task), 1)
+    }
+    else if(taskLists.important.some(task => task.taskId === taskId)){
+        let task = taskLists.important.find(task => task.taskId === taskId)
+        taskLists.important.splice(taskLists.important.indexOf(task), 1)
+    }
+    sessionStorage.setItem('taskLists', JSON.stringify(taskLists))
+    counter =  Number(JSON.parse(sessionStorage.getItem('completedCounter'))) + 1
+    sessionStorage.setItem('completedCounter', JSON.stringify(counter))
+    populateLists(taskLists, true)
+   }
+
    function createUniqueId(){
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
    }
 
-   function populateLists(taskLists){
+   function populateLists(taskLists, increaseCounter){
+    $('.zero-tasks-header').remove()
     $(".not-important-list").empty()
     $(".important-list").empty()
         if(taskLists.important.length > 0){
             for(let task of taskLists.important){
                 $(".important-list").append(createTaskElement(task.taskName, task.taskId))
             }
+        }else{
+            $('<h3 class="zero-tasks-header">No tasks added here yet..</h3>').insertBefore($(".important-list"))
         }
         if(taskLists.notImportant.length > 0){
             for(let task of taskLists.notImportant){
                 $(".not-important-list").append(createTaskElement(task.taskName, task.taskId))
             }
+        }else{
+            $('<h3 class="zero-tasks-header">No tasks added here yet..</h3>').insertBefore($(".not-important-list"))
+        }
+        if(increaseCounter){
+            $('.counter').text(`Completed Tasks: ${counter}`)
         }
    }
 
@@ -143,6 +163,24 @@ $(document).ready(function(){
         editTask(taskId, $('#taskNameEdit').val())
         $("#modal-dialog").css({display: "none"})
     })
+   }
+
+   function initSessionValues(){
+        if(!sessionStorage.getItem('completedCounter')){
+            sessionStorage.setItem('completedCounter', JSON.stringify('0'))
+            counter = 0
+       }else {
+            counter = Number(JSON.parse(sessionStorage.getItem('completedCounter')))
+            console.log(counter)
+       }
+        if(!sessionStorage.getItem('taskLists')){
+            sessionStorage.setItem('taskLists', JSON.stringify({
+                important: [],
+                notImportant: []
+            }))
+        }else {
+        populateLists(JSON.parse(sessionStorage.getItem('taskLists')), true)
+        }
    }
 })
 
